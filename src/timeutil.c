b0eefc17b08ec950c5fe36fd66b120ea5f37add6
Andrea Baldan <a.g.baldan@gmail.com>
2025-03-06 23:22:03 +0000

    Adding time util conversion functions

--- src/timeutil.c
+++ src/timeutil.c
@@ -1,4 +1,10 @@
 #include "timeutil.h"
+#include <string.h>
+
+static const struct {
+    char unit;
+    int mul;
+} units[4] = {{'s', 1}, {'m', 60}, {'h', 60 * 60}, {'d', 24 * 60 * 60}};
 
 long long current_micros(void)
 {
@@ -26,4 +32,33 @@ int clocktime(struct timespec *ts)
 double timespec_seconds(struct timespec *ts)
 {
     return (double)ts->tv_sec + (double)ts->tv_nsec * 1.0e-9;
+}
+
+long long timespan_seconds(long long mul, const char *ts)
+{
+    long long value = -1LL;
+
+    if (strlen(ts) == 2) {
+        if (strncmp(ts, "ns", strlen(ts)) == 0) {
+            value = mul / (1000 * 1000 * 1000);
+        }
+        if (strncmp(ts, "us", strlen(ts)) == 0) {
+            value = mul / (1000 * 1000);
+        }
+        if (strncmp(ts, "ms", strlen(ts)) == 0) {
+            value = mul / 1000;
+        }
+    } else if (strlen(ts) == 1) {
+        for (int i = 0; i < 4; ++i)
+            if (units[i].unit == ts[0])
+                value = mul * units[i].mul;
+    }
+
+    return value;
+}
+
+long long datetime_seconds(const char *dt)
+{
+    // TODO
+    return -1;
 }
--- src/timeutil.h
+++ src/timeutil.h
@@ -7,5 +7,7 @@ long long current_seconds(void);
 long long current_micros(void);
 int clocktime(struct timespec *ts);
 double timespec_seconds(struct timespec *ts);
+long long timespan_seconds(long long mul, const char *ts);
+long long datetime_seconds(const char *dt);
 
 #endif
